c470647c825a1b8adb196c1e7a8551e61775594f
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-18 10:26:44 +0200

    Some edits

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <iostream>
+#include <imgui.h>
+#include <imgui_widgets.cpp>
+#include <imgui_draw.cpp>
+
+int main() {
+
+    std::cout << "Hello ImGui!" << std::endl;
+
+    ImGui::Begin("Title");
+
+    return 0;
+}
